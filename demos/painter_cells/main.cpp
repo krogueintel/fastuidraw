7e04eafcc51fd394615718cb51ec024d170c07fb
Kevin Rogovin <kevin.rogovin@gmail.com>
2019-08-23 14:31:53 +0300

    demos/painter_cells: add command line option to control cell transparency

--- demos/painter_cells/main.cpp
+++ demos/painter_cells/main.cpp
@@ -106,6 +106,7 @@ private:
   command_line_argument_value<bool> m_init_draw_images;
   command_line_argument_value<float> m_init_stroke_width;
   command_line_argument_value<bool> m_init_anti_alias_stroking;
+  command_line_argument_value<bool> m_init_cells_transparent;
 
   CellSharedState m_cell_shared_state;
   TableParams m_table_params;
@@ -218,6 +219,9 @@ painter_cells(void):
   m_init_anti_alias_stroking(true, "init_antialias_stroking",
                              "Initial value for anti-aliasing for stroking",
                              *this),
+  m_init_cells_transparent(false, "init_cells_transparent",
+			   "Initial value for if cells are drawn transparent",
+			   *this),
   m_table(nullptr),
   m_current_blend(0),
   m_show_surface(0),
@@ -439,6 +443,7 @@ derived_init(int w, int h)
   m_cell_shared_state.m_rotating = m_init_cell_rotating.value();
   m_cell_shared_state.m_stroke_width = m_init_stroke_width.value();
   m_cell_shared_state.m_anti_alias_stroking = m_init_anti_alias_stroking.value();
+  m_cell_shared_state.m_draw_transparent = m_init_cells_transparent.value();
   m_cell_shared_state.m_glyph_render = m_renderer.value();
 
   /* init m_zoomer so that table contents fit into screen.
